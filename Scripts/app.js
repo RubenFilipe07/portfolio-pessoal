e77e8bffc638f926eee039804e5c9a77f1252971
Ruben_filipe07 <rubenfilipe.net@gmail.com>
2020-12-26 23:07:35 -0300

    Adicionando arquivos

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+function teste(title){
+    document.getElementById("conhecimentos").style.opacity =  "1" 
+    document.getElementById("conhecimentos").innerText =  title 
+
+}
+
+function limpar(){
+    document.getElementById("conhecimentos").style.opacity =  "0" 
+}
+
