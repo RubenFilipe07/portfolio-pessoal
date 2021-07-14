3f9031d1d6b4e451feaa31178a1a29a75f2fdac7
Rúben Filipe <rubenfilipe.net@gmail.com>
2021-07-14 03:18:29 -0300

    aprimorando desempenho do site

--- recursos/OwlCarousel2/carousel.js
+++ recursos/OwlCarousel2/carousel.js
@@ -2,19 +2,23 @@ $('#carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    lazyLoad:true,
     autoplay:true,
     autoplayTimeout:4000,
     autoplayHoverPause:true,
     responsiveClass:true,
     responsive:{
         0:{
-            items:1
+            items:1,
+            dots:false
         },
         300:{
-            items:4
+            items:4,
+            dots:false
         },
         650:{
-            items:5
+            items:5,
+            dots:true
         }
      }
 });
@@ -28,4 +32,4 @@ function limparNome(){
     document.querySelector(".nome-tecnologias").style.opacity =  "0" 
 }
 
-$('.owl-dots').attr('aria-hidden', 'true');
+$(".owl-dot").attr('aria-label',"own-dot");
